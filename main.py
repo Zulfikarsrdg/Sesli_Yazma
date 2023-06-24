10d87eada9f9e98ed6ee09c921000c7e707cd8c6
Zülfikar Sarıdağ <saridagzulfikar@gmail.com>
2023-06-24 13:59:18 +0300

    Proje Yenilendi

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import os
 import speech_recognition as sr
-r=sr.Recognizer()
+import webbrowser
+
+dinle = sr.Recognizer()
+
 with sr.Microphone() as source:
     print("Lütfen Konuşun")
-    audio=r.listen(source)
+    audio = dinle.listen(source)
 
-data=""
+data = ""
 try:
-    data=r.recognize_google(audio,language='tr-tr')
-    data=data.lower()
-    print("Bunu Söyledin:"+data)
-except sr.UnknownValueError:
-    print("Ne dediğini anlamadım tekrarlar mısın?")
+    data = dinle.recognize_google(audio, language='tr-tr')
+    data = data.lower()
+    print("Bunu Söyledin: " + data)
 
+    if "youtube" or "hepsiburada" or "linkedin" or "github" or "facebook" or "twitter" or "twitch" in data:
+        search_query = data.replace("", "")
+        search_url = "https://www.google.com/search?q=" + search_query
+        webbrowser.open(search_url)
+
+except sr.UnknownValueError:
+    print("Ne dediğini anlamadım, tekrarlar mısın?")
